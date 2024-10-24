e3fb2e74c6d0b23f73df1f3f6cae3dde1728b6ea
luna1860 <saritaprinmc@gmail.com>
2024-10-24 11:01:20 -0500

    more changes

---
+++ SecretConfig.py
@@ -0,0 +1,23 @@
+# Recuerde poner aqui los datos d econexion y guardar como SecretConfig.py
+
+#AQUI VAN LOS DATOS DE TU DB
+#           |    |
+#           |    |
+#           |    |
+#           |    |
+#           |    |
+#       -----    -----
+#       \            /
+#        \          /
+#         \        /
+#          \      /
+#           \    /
+#            \  /
+#             \/
+
+
+PGHOST='ep-winter-poetry-a5ngyyy0.us-east-2.aws.neon.tech'
+PGDATABASE='neondb'
+PGUSER='neondb_owner'
+PGPASSWORD='vPFMeI6bKSX4'
+PGPORT=5432
